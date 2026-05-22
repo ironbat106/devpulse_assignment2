ace482c879ee2e96ddb40648538685cb7e6bb0a2
Aryan Ahad Ateeq <aryanahadateeq02@gmail.com>
2026-05-22 23:10:01 +0600

    added .env

---
+++ src/server.ts
@@ -0,0 +1,57 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import { Pool } from "pg";
+import config from "./config";
+
+const app: Application = express();
+const port = config.port;
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+const pool = new Pool({
+  connectionString: config.connection_string,
+});
+
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        role VARCHAR(20) NOT NULL DEFAULT 'contributor'
+        CHECK(role IN ('contributor', 'maintainer')),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+
+      CREATE TABLE IF NOT EXISTS issues (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(150) NOT NULL,
+        description TEXT NOT NULL,
+        type VARCHAR(30) NOT NULL CHECK(type IN ('bug','feature_request')),
+        status VARCHAR(30) DEFAULT 'open'
+        CHECK(status IN ('open','in_progress','resolved')),
+        reporter_id INT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+    console.log("✅ Database connected & tables ready");
+  } catch (err) {
+    console.error("DB Error:", err);
+  }
+};
+
+initDB();
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
