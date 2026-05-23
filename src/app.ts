9cecf50391c9a44d01f0985746c941b513e250c4
Aryan Ahad Ateeq <aryanahadateeq02@gmail.com>
2026-05-23 12:02:58 +0600

    setting up app.ts

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+import authRoutes from "./modules/auth/auth.route";
+import issueRoutes from "./modules/issues/issue.route";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/* Routes */
+app.use("/api/auth", authRoutes);
+app.use("/api/issues", issueRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "DevPulse API Running" });
+});
+
+export default app;
---
+++ src/modules/issues/issue.route.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  createIssue,
+  getIssues,
+  getSingleIssue,
+  updateIssue,
+  deleteIssue,
+} from "./issue.controller";
+
+import { authMiddleware } from "../../middleware/auth.middleware";
+
+const router = express.Router();
+
+router.post("/", authMiddleware, createIssue);
+router.get("/", getIssues);
+router.get("/:id", getSingleIssue);
+router.patch("/:id", authMiddleware, updateIssue);
+router.delete("/:id", authMiddleware, deleteIssue);
+
+export default router;
